3011de2f04b1b7f701fcb5522b77dffa928760aa
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-06-02 16:23:37 +0700

    add comment

--- src/main/java/com/taidev198/javabackendspringmvc/controller/HomeController.java
+++ src/main/java/com/taidev198/javabackendspringmvc/controller/HomeController.java
@@ -1,2 +1,16 @@
-package com.taidev198.javabackendspringmvc.controller;public class HomeController {
+package com.taidev198.javabackendspringmvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";//find index.jsp file
+    }
+
+
 }
